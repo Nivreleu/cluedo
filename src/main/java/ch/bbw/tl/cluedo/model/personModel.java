a615a6fe00e70d485ec680f9cf01570aa13213c7
Tim <timleuenberger2312@gmail.com>
2023-08-29 12:43:27 +0200

    Created and initilazed personModel and created personService

---
+++ src/main/java/ch/bbw/tl/cluedo/model/personModel.java
@@ -0,0 +1,37 @@
+package ch.bbw.tl.cluedo.model;
+
+public class personModel {
+    private String name;
+    private Integer age;
+    private String hairColor;
+
+    public personModel(String name, Integer age, String hairColor) {
+        this.name = name;
+        this.age = age;
+        this.hairColor = hairColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+}
---
+++ src/main/java/ch/bbw/tl/cluedo/service/personService.java
@@ -0,0 +1,4 @@
+package ch.bbw.tl.cluedo.service;
+
+public class personService {
+}
